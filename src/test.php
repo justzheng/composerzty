45f0f3815159875d8d6e0722560560c844cd1194
zhengtaoyu <717828758@qq.com>
2019-07-22 09:27:20 +0800

    1

---
+++ src/test.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyr
+ * Date: 2019/7/22
+ * Time: 9:21
+ */
+
+namespace cyr\composerzty;
+
+
+class test
+{
+    public $name;
+
+    public $age;
+
+    public function __construct($name,$age)
+    {
+        $this->name = $name;
+        $this->age = $name;
+        echo "123131";
+    }
+}
